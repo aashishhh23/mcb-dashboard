1afaed2f2ea24091eb2da9eaad49459e6e2457e5
aashishhh23 <ashish8803568@gmail.com>
2026-05-06 04:30:37 +0530

    fixed mongoose connection config

--- mcb-backend/config/db.js
+++ mcb-backend/config/db.js
@@ -2,16 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected:", conn.connection.host);
 
   } catch (error) {
     console.error("MongoDB Error:", error.message);
-    process.exit(1); // 🔥 important (fail fast)
+    process.exit(1); // stop server if DB fails
   }
 };
 
