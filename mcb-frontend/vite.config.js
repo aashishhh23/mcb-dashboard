7a741bd1d23fcc444d12fc5fad9599108ca2e962
aashishhh23 <ashish8803568@gmail.com>
2026-05-06 05:38:14 +0530

    final fix: vite base + vercel routing

--- mcb-frontend/vite.config.js
+++ mcb-frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  base: "/", 
+  // WHY:
+  // Vercel par assets (/assets/index.js) sahi path se load ho
+  // warna HTML return hota hai instead of JS → jo ab ho raha hai
 })
